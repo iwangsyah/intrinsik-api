e4cea7e65ff4267f96d37e3bc7837fafe53b0b2a
Wahyu Irwansyah <wahyuiwangsyah@gmail.com>
2021-01-16 10:42:20 +0700

    add read chat

--- routers.js
+++ routers.js
@@ -31,21 +31,22 @@ router.post('/contacts', (req, res) => {
 // -- FROM rooms, users, chat
 // -- WHERE rooms.id_user_1=1 OR rooms.id_user_2=1 AND rooms.last_chat_id=chat.id_chat;
 
-router.post('/chat-rooms', (req, res) => {
+router.post('/chat/rooms', (req, res) => {
+    console.log('rooms');
     const { id } = req.body;
     const sqlQuery = "SELECT distinct rooms.id_room, rooms.id_user_1, rooms.username_1, username_2, rooms.id_user_2, chat.type, chat.message, chat.data, chat.is_read, chat.created_at FROM((rooms INNER JOIN users ON rooms.id_user_1 = " + connection.escape(id) + " OR rooms.id_user_2 = " + connection.escape(id) + ") INNER JOIN chat ON rooms.last_chat_id = chat.id_chat)";
 
     connect(sqlQuery, req, res);
 });
 
-router.get('/chat-list/:id', (req, res) => {
+router.get('/chat/list/:id', (req, res) => {
     const { id } = req.params;
     const sqlQuery = "SELECT * FROM chat WHERE id_room = " + connection.escape(id);
 
     connect(sqlQuery, req, res);
 });
 
-router.post('/chat-list', (req, res) => {
+router.post('/chat/list', (req, res) => {
     const { id_room, id_user, type, data, message, created_at } = req.body;
     const sqlQuery = "INSERT INTO chat (id_room, id_user, type, data, message, created_at) VALUES (" + connection.escape(id_room) + ", " + connection.escape(id_user) + ", " + connection.escape(type) + ", " + connection.escape(data) + ", " + connection.escape(message) + ", " + connection.escape(created_at) + ")";
 
@@ -53,8 +54,11 @@ router.post('/chat-list', (req, res) => {
 });
 
 
-router.put('/chat-list/', (req, res) => {
-    res.send('put chat list')
+router.post('/chat/read/', (req, res) => {
+    const { id } = req.body;
+    const sqlQuery = "UPDATE chat SET is_read = 1 WHERE id_room = " + connection.escape(id);
+
+    connect(sqlQuery, req, res);
 });
 
 router.delete('/chat-list/:id', (req, res) => {
